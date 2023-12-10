a5a55e137312b767447db743f0e384d31ad47fed
Yusuf Fathi <48337380+Yusufathi@users.noreply.github.com>
2023-12-10 15:20:08 +0200

    sss

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from services.registeration_service import RegisterationService
 if __name__ == "__main__":
     reService = RegisterationService()
     registerationModel = reService.getModel()
-
+    
 

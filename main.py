9a35038c3325af0fe7c821cc02d6fa3694f3443f
Yusuf Fathi <48337380+Yusufathi@users.noreply.github.com>
2023-12-19 15:12:36 +0200

    heeeh

--- main.py
+++ main.py
@@ -3,7 +3,34 @@ from services.schedule_service import ScheduleService
 from views.seating_view import SeatingView
 from views.ranges_view import RangesView
 
-if __name__ == "__main__":
+registerationObj = {}
+rangesObj = {}
+
+def buildRoomsLists(examRoom, courseName, courseCode, noOfStudents, examDate, examDay, examTime):
+    courseCode = courseCode.replace(" ","")
+    startID = registerationObj[courseCode][0][0]
+    endID = registerationObj[courseCode][noOfStudents - 1][0]
+    # print(courseCode,courseName,startID,endID)
+    for i in range(noOfStudents):
+        print(registerationObj[courseCode][i])
+        
+
+    # print(f"    Room(s): {examRoom}")
+    # print(f"    No. Of Students: {noOfStudents}")
+    # print(f"    Course Name: {courseName}")
+    # print(f"    Course Code: {courseCode}")
+    # print(f"    Exam Date: {examDate}")
+    # print(f"    Exam Day: {examDay}")
+    # print(f"    Exam Time: {examTime}")
+    # print("==========================================")
+
+def buildExamRange():
+    # sv = SeatingView()
+    # rv = RangesView()
+    pass
+
+
+def iterateOverExamRooms():
     inputScheduleCSVPath = '.\input\input_after_preproccessing.csv'
     scheduleService = ScheduleService(inputFilePath=inputScheduleCSVPath)
     schedule = scheduleService.getModel().scheduleObj
@@ -19,27 +46,11 @@ if __name__ == "__main__":
                 examDate = date.split("_")[0]
                 examDay = date.split("_")[1]
                 examTime = time
-                print(f"    Room(s): {examRoom}")
-                print(f"    No. Of Students: {noOfStudents}")
-                print(f"    Course Name: {courseName}")
-                print(f"    Course Code: {courseCode}")
-                print(f"    Exam Date: {examDate}")
-                print(f"    Exam Day: {examDay}")
-                print(f"    Exam Time: {examTime}")
-                print("==========================================")
+                buildRoomsLists(examRoom,courseName,courseCode,noOfStudents,examDate,examDay,examTime)
                 
-               
-
-        
-    
 
 
-
-
-    # reService = RegisterationService()
-    # registerationModel = reService.getModel()
-    # inputScheduleCSVPath = '.\input\input_after_preproccessing.csv'
-    # scheduleService = ScheduleService(inputFilePath=inputScheduleCSVPath)
-    # sv = SeatingView()
-    # rv = RangesView()
-    
+if __name__ == "__main__":
+    reService = RegisterationService()
+    registerationObj = reService.getModel().subjectsStudentsLists
+    iterateOverExamRooms()
